70dbc1c6e882b9e942c4ce438301341de4b61f67
Maciej Lewandowski <s30391@pjwstk.edu.pl>
2025-06-05 22:57:44 +0200

    Poprawka pet

--- src/backend/pet.py
+++ src/backend/pet.py
@@ -32,10 +32,10 @@ class Pet:
     def sleep(self):
         """Zwierzak spi"""
         self.sleeping = not self.sleeping
-        if self.tired != 100:
-            if self.sleeping:
-                self.tired = min(100.0, self.tired +2)
-                self.exp += 1
+
+        if self.sleeping:
+            self.tired = min(100.0, self.tired +2)
+            self.exp += 1
 
     def eat(self):
         """Karmienie zwierzaka"""
